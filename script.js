546f0958d26df1409243396a41b077f48e3909fe
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-21 01:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var fname=document.getElementById("fname").value;
+	var lname=document.getElementById("lname").value;
+	alert(fname + " " + lname)
 }
